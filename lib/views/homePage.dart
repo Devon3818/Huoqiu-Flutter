0b17ef602a722da9284c5bca2a09b601db48c651
Devon <k849996781@vip.qq.com>
2018-09-08 00:35:25 +0800

    homePage UI

--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../data/goods.dart';
+import '../globalConfig.dart';
  
 class HomePage extends StatefulWidget {
   @override
@@ -9,16 +11,7 @@ class HomePage extends StatefulWidget {
   }
 }
  
-class Page extends State<HomePage> with AutomaticKeepAliveClientMixin {
-  @override
-    void initState() {
-      // TODO: implement initState
-      super.initState();
-      print('HomePageState initState');
-    }
-
-  @override
-    bool get wantKeepAlive => true;
+class Page extends State<HomePage> {
   
   @override
   Widget build(BuildContext context) {
@@ -28,43 +21,82 @@ class Page extends State<HomePage> with AutomaticKeepAliveClientMixin {
   Widget layout(BuildContext context) {
     return new Scaffold(
       appBar: buildAppBar(context),
+      backgroundColor: Colors.white,
       body: new ListView(
         children: <Widget>[
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
-          card(),
+          goodsCard(goodsList[0]),
+          goodsCard(goodsList[1]),
+          goodsCard(goodsList[2]),
+          goodsCard(goodsList[3]),
+          goodsCard(goodsList[0]),
+          goodsCard(goodsList[1]),
+          goodsCard(goodsList[2]),
+          goodsCard(goodsList[3]),
+          goodsCard(goodsList[0]),
+          goodsCard(goodsList[1]),
+          goodsCard(goodsList[2]),
+          goodsCard(goodsList[3]),
+          goodsCard(goodsList[0]),
+          goodsCard(goodsList[1]),
+          goodsCard(goodsList[2]),
+          goodsCard(goodsList[3])
         ],
       )
     );
   }
  
   Widget buildAppBar(BuildContext context) {
-    return new AppBar(title: const Text('推荐', style: const TextStyle(color: Color(0xFF616161))), backgroundColor: Colors.grey[50]);
+    return new AppBar(title: const Text('推荐', style: const TextStyle(color: Color(0xFF616161))), backgroundColor: Colors.white);
   }
- 
-  Widget card() {
-    return new Card(
-      child: new Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        const ListTile(
-            leading: const Icon(Icons.album),
-            title: const Text('The Enchanted Nightingale'),
-            subtitle: const Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
-          )
-        ]
-      )
+
+  Widget goodsCard(Goods goods) {
+    return new Container(
+      child: new FlatButton(
+        padding: const EdgeInsets.all(20.0),
+        onPressed: (){},
+        child: new Column(
+          children: <Widget>[
+            new Container(
+              child: new Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Container(
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage(goods.headUrl),
+                      radius: 18.0
+                    )
+                  ),
+                  new Text(' ' + goods.user , style: new TextStyle(color: Colors.black54)),
+                ]
+              ),
+            ),
+            new Container(
+              padding: const EdgeInsets.only(top: 10.0),                       //宽
+              height: 270.0,
+              decoration: new BoxDecoration(
+                image: new DecorationImage(image: new NetworkImage(goods.banner)),
+                borderRadius: new BorderRadius.all(
+                  const Radius.circular(8.0),
+                ), 
+              )
+            ),
+            new Align(
+              alignment: Alignment.centerLeft,
+              child: new Text(
+                goods.title,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                style: new TextStyle(height: 1.8, color: GlobalConfig.fontColor, fontSize: 20.0, fontWeight: FontWeight.w900,)
+              ),
+            ),
+            new Align(
+              alignment: Alignment.centerLeft,
+              child: new Text(goods.likeNum.toString() + " 点赞 · " + goods.commentNum.toString() + "点评", style: new TextStyle(color: Colors.black38, height: 2.0))
+            )
+          ]
+        ),
+      ),
     );
   }
 }
