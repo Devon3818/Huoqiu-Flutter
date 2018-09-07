4717f8d863d964b6f51b0b700a35c23ea3781303
Devon <k849996781@vip.qq.com>
2018-09-08 00:34:19 +0800

    data

---
+++ lib/data/goods.dart
@@ -0,0 +1,49 @@
+class Goods {
+  String headUrl;
+  String user;
+  String title;
+  String time;
+  String banner;
+  String likeNum;
+  String commentNum;
+
+  Goods(this.headUrl, this.user, this.title, this.time, this.banner, this.likeNum, this.commentNum);
+}
+
+List<Goods> goodsList = [
+  new Goods(
+    'https://img.alicdn.com/tfscom/TB1TP.Ptcj_B1NjSZFHSuuDWpXa', 
+    'B.Duck', 
+    'B.Duck智能保温杯，原创设计潮流品牌', 
+    '5小时前', 
+    'https://img.alicdn.com/tfscom/TB1IAlvtyMnBKNjSZFCSut0KFXa_760x760.jpg', 
+    '64', 
+    '78'
+  ),
+  new Goods(
+    'https://img.alicdn.com/tfscom/TB1W56CtQ7mBKNjSZFySuvydFXa', 
+    'appleye官方企业店', 
+    '近视杀手-appleye全家型护眼训练仪', 
+    '5小时前', 
+    'https://img.alicdn.com/tfscom/TB1s2khtrorBKNjSZFjSuw_SpXa_760x760.jpg', 
+    '64', 
+    '78'
+  ),new Goods(
+    'https://img.alicdn.com/tfscom/TB1XNrrqHZnBKNjSZFGSuvt3FXa', 
+    '7咖啡机', 
+    '7花式咖啡机 专为牛奶咖啡爱好者打造', 
+    '5小时前', 
+    'https://img.alicdn.com/tfscom/TB1ijvAkxtnkeRjSZSgSutAuXXa', 
+    '64', 
+    '78'
+  ),
+  new Goods(
+    'https://img.alicdn.com/tfscom/TB1uR1tIb5YBuNjSspoSuveNFXa', 
+    '好奇研究所', 
+    '网红款水杯 能屈能伸 还能拯救海洋！', 
+    '5小时前', 
+    'https://img.alicdn.com/tfscom/TB1nFYna56guuRjy0FmSuu0DXXa', 
+    '64', 
+    '78'
+  )
+];
