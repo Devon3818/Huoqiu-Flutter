a995c9475f149b6b6f052028568e5c5f4b95e494
Devon <k849996781@vip.qq.com>
2018-09-08 17:17:51 +0800

    goodPage UI

--- lib/views/goodsPage.dart
+++ lib/views/goodsPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../globalConfig.dart';
+import '../data/shop.dart';
  
 class GoodsPage extends StatefulWidget {
   @override
@@ -17,7 +19,24 @@ class Page extends State<GoodsPage> {
  
   Widget layout(BuildContext context) {
     return new Scaffold(
-      appBar: buildAppBar(context)
+      backgroundColor: Colors.white,
+      appBar: buildAppBar(context),
+      body: new ListView(
+        children: <Widget>[
+          card(shoplist[0]),
+          card(shoplist[1]),
+          card(shoplist[2]),
+          card(shoplist[3]),
+          card(shoplist[4]),
+          card(shoplist[5]),
+          card(shoplist[0]),
+          card(shoplist[1]),
+          card(shoplist[2]),
+          card(shoplist[3]),
+          card(shoplist[4]),
+          card(shoplist[5]),
+        ],
+      ),
     );
   }
  
@@ -27,11 +46,91 @@ class Page extends State<GoodsPage> {
       backgroundColor: Colors.grey[50]
     );
   }
- 
-  Widget header(BuildContext context) {
-    return new Image.network(
-      'http://i2.yeyou.itc.cn/2014/huoying/hd_20140925/hyimage06.jpg',
- 
+  
+  Widget card(Shop shop) {
+    return new FlatButton(
+      padding: const EdgeInsets.only(top: 30.0, left: 15.0, right: 15.0),
+      onPressed: (){},
+      child: new Card(
+        elevation: 4.0,
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            new Container(
+              width: 500.00,
+              height: 160.00,
+              child: new FittedBox(
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter,
+                child: new Image.network(shop.banner),
+              )
+            ),
+            new Container(
+              padding: const EdgeInsets.all(14.0),
+              child: new Row(
+                children: <Widget>[
+                  new Container(
+                    child: new CircleAvatar(
+                      backgroundImage: new NetworkImage(shop.logo),
+                      radius: 20.0
+                    )
+                  ),
+                  new Container(
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Container(
+                          child: new Text(
+                            shop.name,
+                            style: new TextStyle(height: 1.5, color: GlobalConfig.fontColor, fontSize: 15.0, fontWeight: FontWeight.w900,)
+                          ),
+                          padding: new EdgeInsets.only(left: 10.0),
+                        ),
+                        new Container(
+                          child: new Row(
+                            children: <Widget>[
+                              new Container(
+                                color: Colors.grey[100],
+                                child: new FlatButton(onPressed: (){}, child: new Text(shop.tag1, style: new TextStyle(fontSize: 10.0)), color: GlobalConfig.searchBackgroundColor),
+                                height: 20.0,
+                                margin: const EdgeInsets.only(right: 8.0)
+                              ),
+                              new Container(
+                                color: Colors.grey[100],
+                                child: new FlatButton(onPressed: (){}, child: new Text(shop.tag2, style: new TextStyle(fontSize: 10.0)), color: GlobalConfig.searchBackgroundColor),
+                                height: 20.0,
+                              ),
+                            ],
+                          ),
+                          padding: new EdgeInsets.only(left: 10.0, top: 4.0, bottom: 0.0, right: 10.0),
+                        ),
+                      ],
+                    )
+                  ),
+                  new Container(
+                    width: 0.7,
+                    height: 35.0,
+                    color: Colors.black12,
+                    margin: EdgeInsets.only(left: 12.0),
+                  ),
+                  new Expanded(
+                    child: new Column(
+                      children: <Widget>[
+                        new Text(
+                          shop.fansNum,
+                          style: new TextStyle(height: 1.5, color: GlobalConfig.fontColor, fontSize: 15.0, fontWeight: FontWeight.w900,)
+                        ),
+                        new Text('粉丝', style: new TextStyle(color: Colors.black38, fontSize: 10.0))
+                      ]
+                    )
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
+  
 }
