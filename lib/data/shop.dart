f309d308b9265b06f8402030e80f14b2666830df
Devon <k849996781@vip.qq.com>
2018-09-08 17:17:38 +0800

    shop data

---
+++ lib/data/shop.dart
@@ -0,0 +1,61 @@
+class Shop {
+  String banner;
+  String name;
+  String logo;
+  String fansNum;
+  String tag1;
+  String tag2;
+
+  Shop(this.banner, this.name, this.logo, this.fansNum, this.tag1, this.tag2);
+}
+
+List<Shop> shoplist = [
+  new Shop(
+    'https://img.alicdn.com/tfscom/TB1zBirD1ySBuNjy1zdSutPxFXa_760x760.jpg',
+    'flyzone运动旗舰店',
+    'https://img.alicdn.com/tfscom/TB1PSndDY1YBuNjSszhSuwUsFXa',
+    '322.5万',
+    '潮流新势',
+    '运动'
+  ),
+  new Shop(
+    'https://img.alicdn.com/tfscom/TB1BShIGXmWBuNjSspdSuvugXXa',
+    '手滑科技',
+    'https://img.alicdn.com/tfscom/TB1UK6bEKuSBuNjSsziSuvq8pXa',
+    '32.5万',
+    '创意科技',
+    '智能体感'
+  ),
+  new Shop(
+    'https://img.alicdn.com/tfscom/TB1kNKOyGSWBuNjSsrbSuu0mVXa',
+    '好福器创意家居',
+    'https://img.alicdn.com/tfscom/TB1UK6bEKuSBuNjSsziSuvq8pXa',
+    '22.5万',
+    '创意家居',
+    '除味器'
+  ),
+  new Shop(
+    'https://img.alicdn.com/tfscom/TB1VMAiCuuSBuNjSsziSuvq8pXa',
+    '艾诗摩尔电热水壶',
+    'https://img.alicdn.com/tfscom/TB1qaaXA7KWBuNjy1zjwu3OypXa',
+    '72.5万',
+    '潮流科技',
+    '创意设计'
+  ),
+  new Shop(
+    'https://img.alicdn.com/tfscom/TB1euxwvyQnBKNjSZFmxKQApVXa_760x760.jpg',
+    '网易春风旗舰店',
+    'https://img.alicdn.com/tfscom/TB19z5fvcUrBKNjSZPxwu000pXa',
+    '16.5万',
+    '成人用品',
+    '春风浪系'
+  ),
+  new Shop(
+    'https://img.alicdn.com/tfscom/TB167_5D7SWBuNjSszdSuveSpXa',
+    '鲶鱼IP',
+    'https://img.alicdn.com/tfscom/TB1qaaXA7KWBuNjy1zjwu3OypXa',
+    '52.5万',
+    '潮流科技',
+    '创意设计'
+  )
+];
